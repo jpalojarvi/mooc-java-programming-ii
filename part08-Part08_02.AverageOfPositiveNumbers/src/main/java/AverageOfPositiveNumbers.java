327836900a1eb44389d01729cdc285d2f0c3028c
Johannes Palojärvi <johannes.palojarvi@gmail.com>
2021-04-20 16:29:32 +0300

    p08-02 completed

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,26 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int numbers = 0;
+        int numbersCount = 0;
+        while (true) {
 
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number > 0) {
+                numbers += number;
+                numbersCount++;
+            }
+            if (number == 0) {
+
+                if (numbersCount > 0) {
+                    System.out.println(1.0*numbers / numbersCount);
+                    break;
+                }
+
+                System.out.println("Cannot calculate the average");
+                break;
+            }
+
+        }
     }
 }
