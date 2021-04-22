e4bf033bf606f2232763b9451e3cea42b9cb847b
Johannes Palojärvi <johannes.palojarvi@gmail.com>
2021-04-22 13:10:16 +0300

    solved

--- part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
+++ part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
@@ -51,4 +51,28 @@ public class SimpleDate {
         return this.year - other.year - yearRemoved;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + this.day;
+        hash = 59 * hash + this.month;
+        hash = 59 * hash + this.year;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SimpleDate other = (SimpleDate) obj;
+        return true;
+    }
+
 }
