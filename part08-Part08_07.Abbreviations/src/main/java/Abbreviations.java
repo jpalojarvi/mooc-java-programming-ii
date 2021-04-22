2f68de7fc8f18ffc15b568c96cf7561bb904c660
Johannes Palojärvi <johannes.palojarvi@gmail.com>
2021-04-22 09:50:40 +0300

    changed class init and constructor to be clearer

--- part08-Part08_07.Abbreviations/src/main/java/Abbreviations.java
+++ part08-Part08_07.Abbreviations/src/main/java/Abbreviations.java
@@ -3,9 +3,10 @@ import java.util.HashMap;
 
 public class Abbreviations {
 
-    private HashMap<String, String> abbreviations = new HashMap<>();
+    private HashMap<String, String> abbreviations;
 
     public Abbreviations(){
+        this.abbreviations = new HashMap<>();
     }
 //    public Abbreviations(String abbreviation, String unAbbreviatiated) {
 //        
