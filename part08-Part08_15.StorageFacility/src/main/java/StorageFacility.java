8811f00235962fd91acbd0ee1f131c924aa7ed60
Johannes Palojärvi <johannes.palojarvi@gmail.com>
2021-04-26 15:04:16 +0300

    part1 completed

---
+++ part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StorageFacility {
+
+    private HashMap<String, ArrayList<String>> storageFacility;
+    
+    public StorageFacility(){
+        this.storageFacility = new HashMap<>();
+    }
+
+    public void add(String unit, String item) {
+        // an empty list has to be added for a new unit if one has not already been added
+        this.storageFacility.putIfAbsent(unit, new ArrayList());
+        
+         // let's first retrieve the list containing the items and add to it
+//         ArrayList<String> itemList = this.storageFacility.get(unit);
+//         itemList.add(item);
+         
+         // the previous would also work without the helper variable as follows
+         this.storageFacility.get(unit).add(item);
+    }
+
+    public ArrayList<String> contents(String storageUnit) {
+        return storageFacility.getOrDefault(storageUnit, new ArrayList<>());
+    }
+
+
+}
