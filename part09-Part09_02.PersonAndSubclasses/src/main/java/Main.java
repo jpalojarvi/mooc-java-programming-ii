97206e7d4fb0204810cbb2f347351ee1ea23dca7
Johannes Palojärvi <johannes.palojarvi@gmail.com>
2021-04-26 16:20:09 +0300

    completed

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -20,7 +22,20 @@ public class Main {
             i = i + 1;
         }
         System.out.println(ollie);
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
+        printPersons(persons);
+        
+        
+    }
+
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person iteratedPerson : persons) {
+            System.out.println(iteratedPerson);
+
+        }
     }
 
 }
