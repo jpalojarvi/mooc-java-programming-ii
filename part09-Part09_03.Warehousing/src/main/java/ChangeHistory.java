2987b3fe8a41e235a1988e0e1fbcad5d21a736d6
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-13 18:28:33 +0300

    p4 complete

--- part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
+++ part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
@@ -22,5 +22,37 @@ public class ChangeHistory {
     public String toString() {
         return this.changeHistory.toString();
     }
+    
+    public double maxValue(){
+        double maxValue = 0.0;
+        for (Double iteratedChange : changeHistory){
+            if (iteratedChange > maxValue){
+                maxValue = iteratedChange;
+            }
+        }
+        return maxValue;
+    }
 
+    public double minValue(){
+        double minValue = changeHistory.get(0);
+        for (Double iteratedChange : changeHistory){
+            if (iteratedChange < minValue){
+                minValue = iteratedChange;
+            }
+        }
+        return minValue;
+    }
+    
+    public double average(){
+        double average = 0.0;
+        double sum = 0.0;
+        int count = 0;
+        for (Double iteratedChange : changeHistory){
+            sum += iteratedChange;
+            count++;
+        }
+        average = sum/count;
+        return average;
+    }
+    
 }
--- part09-Part09_03.Warehousing/src/main/java/Main.java
+++ part09-Part09_03.Warehousing/src/main/java/Main.java
@@ -1,5 +1,4 @@
 
-
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +9,10 @@ public class Main {
         System.out.println(juice.getName());
         juice.addToWarehouse(1.0);
         System.out.println(juice);
+
+        ChangeHistory ch = new ChangeHistory();
+        ch.add(99);
+        ch.minValue();
     }
 
 }
