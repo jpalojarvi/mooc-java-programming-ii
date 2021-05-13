59b294bc47dbcd1ab193a74246e184a6a4a0a52a
Johannes Palojärvi <johannes.palojarvi@gmail.com>
2021-05-13 12:53:48 +0300

    starting to study again, pushing to github to work remotely

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -22,13 +22,11 @@ public class Main {
             i = i + 1;
         }
         System.out.println(ollie);
-        ArrayList<Person> persons = new ArrayList<Person>();
+        ArrayList<Person> persons = new ArrayList<>();
         persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
         persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
         printPersons(persons);
-        
-        
     }
 
     public static void printPersons(ArrayList<Person> persons) {
