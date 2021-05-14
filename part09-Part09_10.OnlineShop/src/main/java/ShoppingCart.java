2f490ba30f1e0278608c9ac1cbc4ca6bc66e0c4b
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-14 21:20:04 +0300

    p5 complete

---
+++ part09-Part09_10.OnlineShop/src/main/java/ShoppingCart.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart {
+
+    private Map<String, Item> contents = new HashMap<>();
+
+    public ShoppingCart() {
+
+    }
+
+    public void add(String product, int price) {
+        Item item = new Item(product, 1, price);
+        contents.put(product, item);
+
+    }
+
+    public int price() {
+        int totalPrice = 0;
+        ArrayList<Item> items = new ArrayList<>();
+        items.addAll(contents.values());
+        for (Item iteratedItem: items){
+            totalPrice += iteratedItem.price();
+        }
+        
+        return totalPrice;
+    }
+}
