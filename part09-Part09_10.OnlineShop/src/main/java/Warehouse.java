e40fa8efe7567b5b55fcfbab3d85b6856823eb2e
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-14 20:39:22 +0300

    p2, p3 solved, exercise is incorrectly giving 4 points though

--- part09-Part09_10.OnlineShop/src/main/java/Warehouse.java
+++ part09-Part09_10.OnlineShop/src/main/java/Warehouse.java
@@ -1,11 +1,12 @@
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Warehouse {
 
-    Map<String, Integer> items = new HashMap<>();
-    Map<String, Integer> prices = new HashMap<>();
+    private Map<String, Integer> items = new HashMap<>();
+    private Map<String, Integer> prices = new HashMap<>();
 
     public void addProduct(String product, int price, int stock) {
         this.items.put(product, stock);
@@ -13,9 +14,31 @@ public class Warehouse {
     }
 
     public int price(String product) {
-        if (this.prices.containsKey(product)) {
-            return prices.get(product);
+//        if (this.prices.containsKey(product)) {
+//            return prices.get(product);
+//        }
+//        return -99;
+        return prices.getOrDefault(product, -99);
+    }
+
+    public int stock(String product) {
+        return items.getOrDefault(product, 0);
+    }
+
+    public boolean take(String product) {
+        if (this.items.containsKey(product)){
+            int stock = items.get(product);
+            if (stock > 0){
+            stock--;
+            this.items.put(product, stock);
+            return true;
+            }
         }
-        return -99;
+        return false;
+    }
+    
+    public Set<String> products(){
+        Set<String> names = this.items.keySet();
+        return names;
     }
 }
