553c97fbc2c167c52daada6dc09d7f92f5f515f5
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-14 20:45:49 +0300

    p4 complete

--- part09-Part09_10.OnlineShop/src/main/java/Main.java
+++ part09-Part09_10.OnlineShop/src/main/java/Main.java
@@ -28,5 +28,11 @@ public class Main {
         for (String product : warehouse.products()) {
             System.out.println(product);
         }
+
+        Item item = new Item("milk", 4, 2);
+        System.out.println("an item that contains 4 milks has the total price of " + item.price());
+        System.out.println(item);
+        item.increaseQuantity();
+        System.out.println(item);
     }
 }
