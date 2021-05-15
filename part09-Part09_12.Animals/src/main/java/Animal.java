a81f94910f57b1b389e3897d4e1bce64aa06a765
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-15 21:46:31 +0300

    p1 solved

---
+++ part09-Part09_12.Animals/src/main/java/Animal.java
@@ -0,0 +1,21 @@
+
+public abstract class Animal {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat() {
+
+    }
+    
+    public void sleep(){
+        
+    }
+}
