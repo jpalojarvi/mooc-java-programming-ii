1bd24fa83c76d5d3a06941b7fa93263300a4936c
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-16 15:48:13 +0300

    while-loop done

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
+        ArrayList<String> inputs = new ArrayList();
+        
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Input numbers, type \"end\" to stop.");
         // Write your program here
-
+        while (true){
+            String input = scanner.nextLine();
+            if (input.equals("end")){
+                break;
+            }
+            inputs.add(input);
+        }
+        
+        
     }
 }
