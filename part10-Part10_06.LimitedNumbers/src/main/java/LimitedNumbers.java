2d27cc2dfcc633252c848c191f26877fcc2d93df
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-17 20:01:47 +0300

    solved

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,18 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> inputs = new ArrayList<>();
 
+        while (true) {
+            String input = scanner.nextLine();
+            int number = Integer.valueOf(input);
+            if (number < 0) {
+                break;
+            }
+            inputs.add(number);
+        }
+        inputs.stream()
+                .filter(predicate -> predicate > 0 && predicate < 6)
+                .forEach(input -> System.out.println(input));
     }
 }
