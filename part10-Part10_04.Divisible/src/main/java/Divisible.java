b0bee0d1138dd1af75991cf51b1bc697e46a199f
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-17 19:10:48 +0300

    solved

--- part10-Part10_04.Divisible/src/main/java/Divisible.java
+++ part10-Part10_04.Divisible/src/main/java/Divisible.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Divisible {
@@ -19,7 +20,10 @@ public class Divisible {
     }
 
     public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
-        return new ArrayList<>();
+        ArrayList<Integer> divisible = numbers.stream()
+                .filter(predicate -> predicate % 2 == 0 | predicate % 3 == 0 | predicate % 5 == 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+        return divisible;
     }
 
 }
