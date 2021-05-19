8a424fc253945dd76fd8c0dd93a23a12d2c49d8c
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-19 14:19:20 +0300

    init 16, added model solution to 15

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -17,5 +17,7 @@ public class Checker {
 
     public boolean timeOfDay(String string) {
         return (string.matches("((([0-1][0-9]|2[0-3]):(([0-5][0-9]|60))*:(([0-6][0-9])|60)*)|24:00:00)"));
+        // model solution
+        //  return string.matches("([01][0123456789]|2[0123]):[012345][0123456789]:[012345][0123456789]");
     }
 }
