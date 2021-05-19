c33dc86b8bf5b5edc6175e1d8ac688d639ba0622
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-19 21:35:31 +0300

    solved

--- part10-Part10_17.SortThemCards/src/main/java/Hand.java
+++ part10-Part10_17.SortThemCards/src/main/java/Hand.java
@@ -3,41 +3,40 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Hand extends BySuitInValueOrder implements Comparable<Hand> {
-
+    
     private ArrayList<Card> hand = new ArrayList<>();
-
+    
     public void add(Card card) {
         this.hand.add(card);
     }
-
+    
     public void print() {
         hand.stream().forEach(card -> System.out.println(card));
     }
-
+    
     public void sort() {
         Comparator<Card> comparator = Comparator
                 .comparing(Card::getValue)
                 .thenComparing(Card::getSuit);
-
+        
         hand.sort(comparator);
     }
-
+    
     @Override
     public int compareTo(Hand o) {
         int thisHandValue = 0;
         int otherHandValue = 0;
-        for (int i = 0; i < this.hand.size(); i++){
+        for (int i = 0; i < this.hand.size(); i++) {
             thisHandValue += this.hand.get(i).getValue();
         }
-        for (int i = 0; i < o.hand.size(); i++){
+        for (int i = 0; i < o.hand.size(); i++) {
             otherHandValue += o.hand.get(i).getValue();
         }
         return thisHandValue - otherHandValue;
     }
     
-    public void sortBySuit(){
+    public void sortBySuit() {
         this.hand.sort(this);
-        
         
     }
 }
