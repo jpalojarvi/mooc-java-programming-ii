f3f91e4cc2514c2be5c5fccba10dbb4d124cfb00
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-19 13:42:30 +0300

    p1 solved

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,10 @@
 
-
 public class Checker {
 
+    public boolean isDayOfWeek(String string) {
+        if (string.matches("mon|tue|wed|thu|fri|sat|sun")) {
+            return true;
+        }
+        return false;
+    }
 }
