00193437d9a2342d8c98244d58ec4ffd32c107b9
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-19 20:26:03 +0300

    p5 solved

--- part10-Part10_17.SortThemCards/src/main/java/Main.java
+++ part10-Part10_17.SortThemCards/src/main/java/Main.java
@@ -1,24 +1,40 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
         // test your code here
 
-        Card first = new Card(2, Suit.DIAMOND);
-        Card second = new Card(14, Suit.SPADE);
-        Card third = new Card(12, Suit.HEART);
+//        Card first = new Card(2, Suit.DIAMOND);
+//        Card second = new Card(14, Suit.SPADE);
+//        Card third = new Card(12, Suit.HEART);
+//
+//        System.out.println(first);
+//        System.out.println(second);
+//        System.out.println(third);
+//
+//        Hand hand = new Hand();
+//
+//        hand.add(new Card(2, Suit.DIAMOND));
+//        hand.add(new Card(14, Suit.SPADE));
+//        hand.add(new Card(12, Suit.HEART));
+//        hand.add(new Card(2, Suit.SPADE));
+//
+//        hand.print();
 
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
+        ArrayList<Card> cards = new ArrayList<>();
 
-        Hand hand = new Hand();
+        cards.add(new Card(3, Suit.SPADE));
+        cards.add(new Card(2, Suit.DIAMOND));
+        cards.add(new Card(14, Suit.SPADE));
+        cards.add(new Card(12, Suit.HEART));
+        cards.add(new Card(2, Suit.SPADE));
 
-        hand.add(new Card(2, Suit.DIAMOND));
-        hand.add(new Card(14, Suit.SPADE));
-        hand.add(new Card(12, Suit.HEART));
-        hand.add(new Card(2, Suit.SPADE));
+        BySuitInValueOrder sortBySuitSorter = new BySuitInValueOrder();
+        Collections.sort(cards, sortBySuitSorter);
 
-        hand.print();
+        cards.stream().forEach(c -> System.out.println(c));
     }
 }
