f50973d74898110553eaf43b8c20846c81256c75
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-19 19:26:46 +0300

    p4 solved

--- part10-Part10_17.SortThemCards/src/main/java/Hand.java
+++ part10-Part10_17.SortThemCards/src/main/java/Hand.java
@@ -2,23 +2,36 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 
+public class Hand implements Comparable<Hand> {
 
-public class Hand {
     private ArrayList<Card> hand = new ArrayList<>();
-    
-    public void add(Card card){
+
+    public void add(Card card) {
         this.hand.add(card);
     }
-  
-    public void print(){
+
+    public void print() {
         hand.stream().forEach(card -> System.out.println(card));
     }
-    
-    public void sort(){
+
+    public void sort() {
         Comparator<Card> comparator = Comparator
                 .comparing(Card::getValue)
                 .thenComparing(Card::getSuit);
-        
+
         hand.sort(comparator);
+    }
+
+    @Override
+    public int compareTo(Hand o) {
+        int thisHandValue = 0;
+        int otherHandValue = 0;
+        for (int i = 0; i < this.hand.size(); i++){
+            thisHandValue += this.hand.get(i).getValue();
+        }
+        for (int i = 0; i < o.hand.size(); i++){
+            otherHandValue += o.hand.get(i).getValue();
+        }
+        return thisHandValue - otherHandValue;
     }
 }
