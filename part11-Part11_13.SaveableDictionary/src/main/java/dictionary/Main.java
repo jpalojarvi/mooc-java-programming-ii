016f72bd92697291e7fa79d8ac41f7b9ce1b7ecf
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-05-22 23:32:17 +0300

    solved

--- part11-Part11_13.SaveableDictionary/src/main/java/dictionary/Main.java
+++ part11-Part11_13.SaveableDictionary/src/main/java/dictionary/Main.java
@@ -17,15 +17,41 @@ public class Main {
 //        System.out.println(dictionary.translate("banaani"));
 //        System.out.println(dictionary.translate("ohjelmointi"));
 
-        SaveableDictionary dictionary = new SaveableDictionary("word.txt");
-        boolean wasSuccessful = dictionary.load();
+//        SaveableDictionary dictionary = new SaveableDictionary("word.txt");
+//        boolean wasSuccessful = dictionary.load();
+//
+//        if (wasSuccessful) {
+//            System.out.println("Successfully loaded the dictionary from file");
+//        }
+//
+//        System.out.println(dictionary.translate("apina"));
+//        System.out.println(dictionary.translate("ohjelmointi"));
+//        System.out.println(dictionary.translate("alla oleva"));
+//        SaveableDictionary s = new SaveableDictionary();
+//        s.add("apina", "monkey");
+//        s.add("tietokone", "computer");
+//        System.out.println(s.translate("monkey"));
+//// expected:<apina> but was:<null>
 
-        if (wasSuccessful) {
-            System.out.println("Successfully loaded the dictionary from file");
-        }
+//FAIL: dictionary.SaveableDictionaryTest changesToTheExistingFileGetSaved
+//When the file's word-5476.txt contents are:
+//apina:monkey
+//alla oleva:below
+//olut:beer
 
-        System.out.println(dictionary.translate("apina"));
-        System.out.println(dictionary.translate("ohjelmointi"));
-        System.out.println(dictionary.translate("alla oleva"));
+//Saved with the code
+SaveableDictionary s = new SaveableDictionary("word-5476.txt");
+s.load();
+s.delete("below");
+s.add("tietokone", "computer");
+s.save();
+//the following was saved to a file
+//--
+//alla oleva:below
+//tietokone:computer
+//apina:monkey
+//olut:beer
+//--
+//the amount of lines was incorrect expected:<3> but was:<4>
     }
 }
--- part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
+++ part11-Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -2,13 +2,19 @@ package dictionary;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Map;
+;
 import java.util.Scanner;
+
+
 
 public class SaveableDictionary {
 
     private HashMap<String, String> dictionary = new HashMap<>();
+    private HashMap<String, String> dictionaryReversed = new HashMap<>();
     private String filename;
 
     public SaveableDictionary() {
@@ -20,14 +26,13 @@ public class SaveableDictionary {
 
     public boolean load() {
         try {
-            // FileWriter fileWriter = new FileWriter(this.filename);
             Scanner fileReader = new Scanner(Paths.get(filename));
 
             while (fileReader.hasNextLine()) {
                 String line = fileReader.nextLine();
                 String[] parts = line.split(":");
                 this.dictionary.putIfAbsent(parts[0], parts[1]);
-                this.dictionary.putIfAbsent(parts[1], parts[0]);
+                this.dictionaryReversed.putIfAbsent(parts[1], parts[0]);
             }
 
         } catch (Exception e) {
@@ -36,22 +41,45 @@ public class SaveableDictionary {
         return true;
     }
 
-    public void add(String words, String translation) {
-        this.dictionary.putIfAbsent(words, translation);
-        this.dictionary.putIfAbsent(translation, words);
+    public boolean save() {
+        try {
+            PrintWriter printWriter = new PrintWriter(this.filename);
+            for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+                printWriter.println(entry.getKey() + ":" + entry.getValue());
+            }
+            printWriter.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void add(String word, String translation) {
+        this.dictionary.putIfAbsent(word, translation);
+        this.dictionaryReversed.putIfAbsent(translation, word);
     }
 
     public String translate(String word) {
-        return this.dictionary.getOrDefault(word, null);
+        if (this.dictionary.containsKey(word)) {
+            return this.dictionary.get(word);
+        } else if (this.dictionaryReversed.containsKey(word)) {
+            return this.dictionaryReversed.get(word);
+        } else {
+            return null;
+        }
     }
 
     public void delete(String word) {
-        String valueToBeRemoved;
-        if (this.dictionary.containsValue(word)) {
-            valueToBeRemoved = this.dictionary.get(word);
-            this.dictionary.remove(valueToBeRemoved);
-        }
-        this.dictionary.remove(word);
 
+        if (this.dictionary.containsKey(word)) {
+            String correspondingValue = dictionary.get(word);
+            this.dictionaryReversed.remove(correspondingValue);
+            this.dictionary.remove(word);
+        } else if (dictionaryReversed.containsKey(word)) {
+            String correspondingValue = dictionaryReversed.get(word);
+            this.dictionary.remove(correspondingValue);
+            this.dictionaryReversed.remove(word);
+
+        }
     }
 }
