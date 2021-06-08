b6c450631fe52289870e90f4f121614679a6d615
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-06-08 19:23:44 +0300

    solved

--- part12-Part12_04.List/src/main/java/Program.java
+++ part12-Part12_04.List/src/main/java/Program.java
@@ -6,7 +6,30 @@ public class Program {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // test your class here
+//        List<String> myList = new List<>();
+//        myList.add("hello");
+//        myList.add("world");
 
+//        List<String> myList = new List<>();
+//        for (int i = 0; i < 11; i++) {
+//            myList.add("hello");
+//        }
+//        List<String> myList = new List<>();
+//        System.out.println(myList.contains("hello"));
+//        myList.add("hello");
+//        System.out.println(myList.contains("hello"));
+//        int index = myList.indexOfValue("hello");
+//        System.out.println(index);
+//        System.out.println(myList.value(index));
+//        myList.remove("hello");
+//        System.out.println(myList.contains("hello"));
+        List<String> myList = new List<>();
+        myList.add("hello");
+        myList.add("world");
+
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.value(i));
+        }
     }
 
 }
